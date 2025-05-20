ece3ec096057af6ec04b7a76a8f86e223deee5d4
Aniruth-potter <101163323+Aniruth-potter@users.noreply.github.com>
2025-05-20 22:02:05 +0530

    microservice

--- accountservice/src/main/java/com/bank/accountservice/AccountserviceApplication.java
+++ accountservice/src/main/java/com/bank/accountservice/AccountserviceApplication.java
@@ -1,14 +1,17 @@
 package com.bank.accountservice;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication(scanBasePackages = "com.bank")
 @EnableJpaRepositories(basePackages = "com.bank.repos")
 @EntityScan(basePackages = "com.bank.entity")
+@EnableDiscoveryClient
 public class AccountserviceApplication {
 
 	public static void main(String[] args) {
